24d801a6cbe55d6417a3961c133256f7e37e09b5
Kakalasek <example@email.com>
2025-02-01 12:46:02 +0100

    Account balance

--- src/main/java/com/BankServer/CommandController/BankCommands/BalanceAccountCommand.java
+++ src/main/java/com/BankServer/CommandController/BankCommands/BalanceAccountCommand.java
@@ -1,7 +1,13 @@
 package com.BankServer.CommandController.BankCommands;
 
 import com.BankServer.Bank.Bank;
+import com.google.common.net.InetAddresses;
 
+import java.security.InvalidParameterException;
+
+/**
+ * Used to obtain the balance of a particular account
+ */
 public class BalanceAccountCommand implements BankCommand {
 
     private final Bank bank;
@@ -10,10 +16,26 @@ public class BalanceAccountCommand implements BankCommand {
         this.bank = bank;
     }
 
+    /**
+     * @param args args[0] ... accountNumber/bankCode(ipv4 address)
+     * @return The balance of the account
+     * @throws InvalidParameterException If there is any problem with the provided arguments
+     */
     @Override
     public String execute(String[] args) {
-        String account = args[0].split("/")[0];
-        String ip = args[0].split("/")[1];
+
+        String[] accountAndIp = args[0].split("/");
+
+        if(accountAndIp.length != 2){
+            throw new InvalidParameterException("You have to provide your account number and ip separated by /");
+        }
+
+        String account = accountAndIp[0];
+        String ip = accountAndIp[1];
+
+        if(!account.matches("\\d+")) throw new InvalidParameterException("The account number must be a natural number");
+        if(!bank.isAccountNumber(Integer.parseInt(account))) throw new InvalidParameterException("This account number cant exist in this bank");
+        if(!InetAddresses.isInetAddress(ip)) throw new InvalidParameterException("The bank code must be a valid ip address");
 
         return String.valueOf(bank.getAccountBalance(Integer.parseInt(account)));
     }
